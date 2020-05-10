fb036bddfb4b7f219790cc2bc8ad077d368055fc
parvizrovshanaliyev <parvizra@code.edu.az>
2020-05-10 21:29:59 +0400

    basic auth okay

--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Negotiate;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,14 @@ namespace Basic
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth",config =>
+                    {
+                        config.Cookie.Name = "Basics.Cookie";
+                        config.LoginPath = "/Home/Authenticate";
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +36,18 @@ namespace Basic
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
+
+            // who are you ?
+            app.UseAuthentication();
+            // are you allow ?
+            app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
