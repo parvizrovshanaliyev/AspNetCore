8d6b02c21fb836b2754003e4ec60e316ed9c9d6a
parvizrovshanaliyev <parvizra@code.edu.az>
2020-07-02 16:36:25 +0400

    api

--- CRUDAPI/Startup.cs
+++ CRUDAPI/Startup.cs
@@ -1,24 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace Basic
+namespace CRUDAPI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
-            services.AddAuthentication("CookieAuth")
-                .AddCookie("CookieAuth",config =>
-                    {
-                        config.Cookie.Name = "Basics.Cookie";
-                        config.LoginPath = "/Home/Authenticate";
-                    });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,18 +36,15 @@ namespace Basic
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            // who are you ?
-            app.UseAuthentication();
-            // are you allow ?
             app.UseAuthorization();
-            
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers();
             });
         }
     }
