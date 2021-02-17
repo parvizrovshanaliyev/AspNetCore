12dd1d647276c83943c43fe960bd7bf3e2493828
parvizrovshanaliyev <parvizra@code.edu.az>
2021-02-17 19:35:15 +0400

    Actions 15 ci ders`

--- Example1/Controllers/ProductController.cs
+++ Example1/Controllers/ProductController.cs
@@ -3,14 +3,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Example1.Models;
 
 namespace Example1.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+
+        #region ViewResult
+
+        //public ViewResult Index()
+        //{
+        //    return View();
+        //}
+
+        #endregion
+        #region PartialViewResult
+
+        //public PartialViewResult Index()
+        //{
+        //    return PartialView();
+        //}
+
+        #endregion
+        #region JsonResult
+
+        //public JsonResult Index()
+        //{
+        //    return Json(new Product{Id = 1,Name = "iphone",Quantity = 100});
+        //}
+
+        #endregion
+        #region EmptyResult
+
+        //public EmptyResult Index()
+        //{
+        //    return new EmptyResult();
+        //}
+
+        #endregion
+        #region ContentResult
+
+        //public ContentResult Index()
+        //{
+        //    return Content("hello world");
+        //}
+
+        #endregion
+        #region ActionResult
+
+        public ActionResult Index() // base action sayilir
         {
-            return View();
+            return Content("hello world");
         }
+
+        #endregion
     }
+
 }
