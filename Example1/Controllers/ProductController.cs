bb47c405a681a92460941689ce56f5467b0aa5ec
parvizrovshanaliyev <parvizra@code.edu.az>
2021-02-20 16:45:32 +0400

    Model binding ders 24 start

--- Example1/Controllers/ProductController.cs
+++ Example1/Controllers/ProductController.cs
@@ -10,54 +10,7 @@ namespace Example1.Controllers
     public class ProductController : Controller
     {
 
-        #region ViewResult
-
-        //public ViewResult Index()
-        //{
-        //    return View();
-        //}
-
-        #endregion
-        #region PartialViewResult
-
-        //public PartialViewResult Index()
-        //{
-        //    return PartialView();
-        //}
-
-        #endregion
-        #region JsonResult
-
-        //public JsonResult Index()
-        //{
-        //    return Json(new Product{Id = 1,Name = "iphone",Quantity = 100});
-        //}
-
-        #endregion
-        #region EmptyResult
-
-        //public EmptyResult Index()
-        //{
-        //    return new EmptyResult();
-        //}
-
-        #endregion
-        #region ContentResult
-
-        //public ContentResult Index()
-        //{
-        //    return Content("hello world");
-        //}
-
-        #endregion
-        #region ActionResult
-
-        public ActionResult Index() // base action sayilir
-        {
-            return Content("hello world");
-        }
-
-        #endregion
+        
     }
 
 }
