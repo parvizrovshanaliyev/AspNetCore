1366761c554aee5b281d6d9ba4517c4d382173d0
parvizrovshanaliyev <parvizra@code.edu.az>
2021-06-12 20:45:52 +0400

    Client terefinde signalR connection qurulmasi , statuslara gore ferqli emeliyyatlarini yerine yetirilmesi , connection functions

---
+++ src/SignalR_Examples/SignalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExample.Hubs
+{
+    /*
+     * 1.appde oncelikle sonunda HUB deye adlandirilan class
+     * yaradilir ve HUB classindan inheritance alir bununla
+     * TCP protokolundan istifade edeceyik.
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+    public class MyHub:Hub
+    {
+        /// <summary>
+        /// 2.message parametri qebul eden bu method icerisinde Hub-dan gelen
+        /// Clients prop vasitesile .All butun istifadecilerde "receiveMessage"
+        /// methodunu ise salib qebul olunan message param-i olara gonderirik
+        ///
+        /// 3. Goto StartUp class
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessageAsync(string message)
+        {
+           await  Clients.All.SendAsync("receiveMessage",message);
+        }
+    }
+}
--- src/SignalR_Examples/SignalRServerExample/Startup.cs
+++ src/SignalR_Examples/SignalRServerExample/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRServerExample.Hubs;
 
 namespace SignalRServerExample
 {
@@ -16,6 +17,18 @@ namespace SignalRServerExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //3. signal module elave edirik
+            services.AddSignalR();
+
+            // 5. requestleri qebul etmek ucun addCors politikasini elave etmeliyik
+            services.AddCors(options => options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .SetIsOriginAllowed(origin=>true);
+
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +38,16 @@ namespace SignalRServerExample
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            /* 5. requestleri qebul etmek ucun addCors politikasini elave etmeliyik
+             *   app.UseRouting();-den once .UseCors() middleware cagrilmalidir
+            */
+            app.UseCors();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // 4. https://localhost:5001
+                endpoints.MapHub<MyHub>("/myhub");
             });
         }
     }
