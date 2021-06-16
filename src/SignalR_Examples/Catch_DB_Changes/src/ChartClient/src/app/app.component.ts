e2f68a298b7e37eb884b2f397fe02a8bbdafa009
parvizrovshanaliyev <parvizra@code.edu.az>
2021-06-16 10:34:37 +0400

    app component silindi

--- src/SignalR_Examples/Catch_DB_Changes/src/ChartClient/src/app/app.component.ts
+++ src/SignalR_Examples/Catch_DB_Changes/src/ChartClient/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HighchartsChartComponent } from 'highcharts-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
